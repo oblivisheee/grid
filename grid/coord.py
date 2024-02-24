d59cedc357ceda87c8bf9dff7de69a1a6a41a0d0
oblivisheee <molniya213y@gmail.com>
2024-02-23 20:29:54 -0800

    Update coord.py

--- grid/coord.py
+++ grid/coord.py
@@ -1,9 +1,10 @@
 from typing import Tuple
 
 def auto_coordinates(grid) -> Tuple[int, int, int]:
+    _shape = grid.shape
     for x in range(grid.shape[0]):
         for y in range(grid.shape[1]):
             for z in range(grid.shape[2]):
                 if grid[x, y, z] is None:
                     return (x, y, z)
-    return (grid.shape[0]-1, grid.shape[1]-1, grid.shape[2]-1)
+    return (_shape[0]-1, _shape[1]-1, _shape[2]-1)
